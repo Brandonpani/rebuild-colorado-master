5bfffec67607370801cb7f250eb27ac0a48faf0d
Brandon <bbrandonpaniagua@gmail.com>
2022-05-21 20:28:54 -0600

    modal open

--- dist/app.js
+++ dist/app.js
@@ -3,5 +3,9 @@ const modalContainer = document.getElementById('modalContainer');
 const modalClose = document.getElementById('modalClose');
 
 modalOpen.addEventListener('click', () => {
-    modalContainer.classList.toggle('Show');
+    modalContainer.classList.add('show');
+    // console.log(modalOpen);
+});
+modalClose.addEventListener('click', () => {
+    modalContainer.classList.remove('show');
 });
